91df44e0912dae41c729f2a7ffef42221b1fc331
Shashi Nagarajan Iyer <shashiniyer@gmail.com>
2020-09-13 18:37:36 +0200

    Added QR method

--- R/linreg.R
+++ R/linreg.R
@@ -3,36 +3,59 @@ linreg <- function(x, ...) UseMethod("linreg")
 
 # create specific methods
 
-linreg.formula <- function(formula, data, add_intercept = TRUE) {
+linreg.formula <- function(formula, data, add_intercept = TRUE, qr_method = FALSE) {
   
   # process data
   processed_data <- model.matrix(data, formula, add_intercept)
   y <- processed_data$y # dependent (response) variable
   X <- processed_data$X # design matrix
+  
+  # check for design matrix invertibility issues
+  invert_issue <- suppressWarnings(!(class(try(solve(t(X) %*% X), silent = FALSE))[1] == "matrix"))
+  
+  # calculate output
 
-  # assemble output
-  call <- paste0("linreg(formula = ", deparse(formula), ", data = ", deparse(substitute(data)), ")")
-  coefficients <- solve(t(X) %*% X) %*% t(X) %*% y
-  names(coefficients) <- colnames(X)
-  preds <- X %*% coefficients
-  residuals <- y - preds
-  df <- nrow(X) - ncol(X)
-  residual_variance <- as.numeric((t(residuals) %*% residuals) / df)
-  variance_coeff <- residual_variance * solve(t(X) %*% X)
+  if (qr_method == FALSE & !invert_issue) { # without QR
+
+    coefficients <- solve(t(X) %*% X) %*% t(X) %*% y
+    names(coefficients) <- colnames(X)
+    preds <- X %*% coefficients
+    residuals <- y - preds
+    df <- nrow(X) - ncol(X)
+    residual_variance <- as.numeric((t(residuals) %*% residuals) / df)
+    variance_coeff <- residual_variance * solve(t(X) %*% X)
+    
+  } else { # with QR
+  
+    if (invert_issue) {"*** Using QR Method for solution ***"}
+    df <- nrow(X) - ncol(X)
+    X <- qr(X)
+    R <- qr.R(X)
+    
+    coefficients <- qr.coef(X, y)
+    preds <- qr.fitted(X, y)
+    residuals <- y - preds
+    residual_variance <- as.numeric((t(residuals) %*% residuals) / df)
+    variance_coeff <- residual_variance * solve(t(R) %*% R)
+    
+  }
+  
   t_values <- coefficients / sqrt(diag(variance_coeff))
   p_values <- sapply(2*(1 - pt(abs(t_values), df)), function(x) {if(x < 2e-16) {"<2e-16"} else x})
   
-  return_object <- list(call = call, coefficients = coefficients, fitted_values = preds,
+  # assemble and return output object
+  return_object <- list(call = match.call(), coefficients = coefficients, fitted_values = preds,
                         residuals = residuals, df = df, residual_variance = residual_variance,
                         variance_of_coefficients = variance_coeff, t_values = t_values, p_values = p_values)
   class(return_object) <- "linreg"
   
-  # return output
   return(return_object)
 }
 
 print.linreg <- function(x, ...) {
-  cat("Call:\n", x$call,"\n\nCoefficients:\n")
+  cat("Call:\n")
+  print(x$call)
+  cat("\n\nCoefficients:\n")
   named_coeff_vector <- as.vector(x$coefficients)
   names(named_coeff_vector) <- names(x$coefficients)
   print(named_coeff_vector)
