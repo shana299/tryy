a51888379e9b39bacb9b10d77ca2bf0c5d04b366
Shashi Nagarajan Iyer <shashiniyer@gmail.com>
2020-09-14 09:05:21 +0200

    clean up plot.linreg

--- R/linreg.R
+++ R/linreg.R
@@ -1,3 +1,5 @@
+#' @import ggplot2
+
 # create generic functions needed
 linreg <- function(x, ...) UseMethod("linreg")
 
@@ -63,8 +65,7 @@ print.linreg <- function(x, ...) {
 
 plot.linreg <- function(x, num_labels = 3, labels_by_val = FALSE, ...) {
   
-  # import ggplot2 library using the call usethis::use_package("ggplot2")
-  # this will prompt roxygen2 to add ggplot2 to the Imports field in DESCRIPTION
+  require(ggplot2) # needing to use this despite adding ggplot2 to DESCRIPTION file under Imports via usethis::use_package("ggplot2")
   
   df_for_plot <- data.frame(fitted_values = x$fitted_values, residuals = x$residuals)
   top_n <- order(abs(x$residuals), decreasing = TRUE)[1:num_labels]
@@ -73,18 +74,20 @@ plot.linreg <- function(x, num_labels = 3, labels_by_val = FALSE, ...) {
 
   if (!labels_by_val) {
     
-    ggplot(df_for_plot, aes(x=fitted_values, y=residuals)) +
-      geom_smooth(method="loess", aes(colour="red"), se = FALSE) +
+    p1 <- ggplot(df_for_plot, aes(x=fitted_values, y=residuals)) +
+      geom_smooth(method="loess", aes(colour="red"), se = FALSE, show.legend = FALSE, formula = y~x) +
       geom_point(shape = 1) + labs(title="Residuals Vs Fitted", y="Residuals", x="Fitted values") +
       theme(plot.title = element_text(hjust = 0.5)) + theme_classic() + 
+      geom_hline(yintercept = 0, linetype="dotted") + 
       geom_text(data=df_for_plot[top_n, ], aes(label=rownames(df_for_plot[top_n, ])), nudge_x = 0.2)
     
   } else {
     
-    ggplot(df_for_plot, aes(x=fitted_values, y=residuals, label=residuals)) +
-      geom_smooth(method="loess", aes(colour="red"), se = FALSE) +
+    p1 <- ggplot(df_for_plot, aes(x=fitted_values, y=residuals, label=residuals)) +
+      geom_smooth(method="loess", aes(colour="red"), se = FALSE, show.legend = FALSE, formula = y~x) +
       geom_point(shape = 1) + labs(title="Residuals Vs Fitted", y="Residuals", x="Fitted values") +
       theme(plot.title = element_text(hjust = 0.5)) + theme_classic() +
+      geom_hline(yintercept = 0, linetype="dotted") + 
       geom_text(data=df_for_plot[top_n, ], nudge_x = 0.2)
   
   }
@@ -95,22 +98,24 @@ plot.linreg <- function(x, num_labels = 3, labels_by_val = FALSE, ...) {
   
   if (!labels_by_val) {
     
-    ggplot(df_for_plot, aes(x=fitted_values, y=sqrt(abs(std_res)))) +
-      geom_smooth(method="loess", aes(colour="red"), se = FALSE) +
-      geom_point(shape = 1) + labs(title="Scale-Location", y=expression(sqrt(abs(std_res))), x="Fitted values") +
+    p2 <- ggplot(df_for_plot, aes(x=fitted_values, y=sqrt(abs(std_res)))) + geom_point(shape = 1) +
+      geom_smooth(method="loess", aes(colour="red"), se = FALSE, show.legend = FALSE, formula = y~x) + 
+      labs(title="Scale-Location", y=expression(sqrt(abs(paste("Standardised ","Residuals")))), x="Fitted values") +
       theme(plot.title = element_text(hjust = 0.5)) + theme_classic() + 
       geom_text(data=df_for_plot[top_n, ], aes(label=rownames(df_for_plot[top_n, ])), nudge_x = 0.2)
     
   } else {
     
-    ggplot(df_for_plot, aes(x=fitted_values, y=sqrt(abs(std_res)))) +
-      geom_smooth(method="loess", aes(colour="red"), se = FALSE) +
-      geom_point(shape = 1) + labs(title="Scale-Location", y=expression(sqrt(abs(std_res))), x="Fitted values") +
+    p2 <- ggplot(df_for_plot, aes(x=fitted_values, y=sqrt(abs(std_res)))) + geom_point(shape = 1) +
+      geom_smooth(method="loess", aes(colour="red"), se = FALSE, show.legend = FALSE, formula = y~x) + 
+      labs(title="Scale-Location", y=expression(sqrt(abs(paste("Standardised ","Residuals")))), x="Fitted values") +
       theme(plot.title = element_text(hjust = 0.5)) + theme_classic() + 
       geom_text(data=df_for_plot[top_n, ], nudge_x = 0.2)
     
   }
   
+  suppressWarnings(plot(p1))
+  suppressWarnings(plot(p2))
     
 }
 
