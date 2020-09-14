46e08025a5ff70f58b61184455590bfed541f522
Shashi Nagarajan Iyer <shashiniyer@gmail.com>
2020-09-14 08:13:40 +0200

    changed package name from linear_regression (illegal) to linear.regression (DESCRIPTION) + added plot.linreg method

--- R/linreg.R
+++ R/linreg.R
@@ -61,12 +61,57 @@ print.linreg <- function(x, ...) {
   print(named_coeff_vector)
 }
 
-plot.linreg <- function(x, ...) {
+plot.linreg <- function(x, num_labels = 3, labels_by_val = FALSE, ...) {
   
   # import ggplot2 library using the call usethis::use_package("ggplot2")
   # this will prompt roxygen2 to add ggplot2 to the Imports field in DESCRIPTION
   
+  df_for_plot <- data.frame(fitted_values = x$fitted_values, residuals = x$residuals)
+  top_n <- order(abs(x$residuals), decreasing = TRUE)[1:num_labels]
   
+  # plot Residuals Vs Fitted
+
+  if (!labels_by_val) {
+    
+    ggplot(df_for_plot, aes(x=fitted_values, y=residuals)) +
+      geom_smooth(method="loess", aes(colour="red"), se = FALSE) +
+      geom_point(shape = 1) + labs(title="Residuals Vs Fitted", y="Residuals", x="Fitted values") +
+      theme(plot.title = element_text(hjust = 0.5)) + theme_classic() + 
+      geom_text(data=df_for_plot[top_n, ], aes(label=rownames(df_for_plot[top_n, ])), nudge_x = 0.2)
+    
+  } else {
+    
+    ggplot(df_for_plot, aes(x=fitted_values, y=residuals, label=residuals)) +
+      geom_smooth(method="loess", aes(colour="red"), se = FALSE) +
+      geom_point(shape = 1) + labs(title="Residuals Vs Fitted", y="Residuals", x="Fitted values") +
+      theme(plot.title = element_text(hjust = 0.5)) + theme_classic() +
+      geom_text(data=df_for_plot[top_n, ], nudge_x = 0.2)
+  
+  }
+
+  # plot Scale-Location
+  
+  df_for_plot$std_res <- (df_for_plot$residual - mean(df_for_plot$residual))/sd(df_for_plot$residual)
+  
+  if (!labels_by_val) {
+    
+    ggplot(df_for_plot, aes(x=fitted_values, y=sqrt(abs(std_res)))) +
+      geom_smooth(method="loess", aes(colour="red"), se = FALSE) +
+      geom_point(shape = 1) + labs(title="Scale-Location", y=expression(sqrt(abs(std_res))), x="Fitted values") +
+      theme(plot.title = element_text(hjust = 0.5)) + theme_classic() + 
+      geom_text(data=df_for_plot[top_n, ], aes(label=rownames(df_for_plot[top_n, ])), nudge_x = 0.2)
+    
+  } else {
+    
+    ggplot(df_for_plot, aes(x=fitted_values, y=sqrt(abs(std_res)))) +
+      geom_smooth(method="loess", aes(colour="red"), se = FALSE) +
+      geom_point(shape = 1) + labs(title="Scale-Location", y=expression(sqrt(abs(std_res))), x="Fitted values") +
+      theme(plot.title = element_text(hjust = 0.5)) + theme_classic() + 
+      geom_text(data=df_for_plot[top_n, ], nudge_x = 0.2)
+    
+  }
+  
+    
 }
 
 resid.linreg <- function(x, ...) {x$residuals}
