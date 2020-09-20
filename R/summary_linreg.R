435972db306d8c8771eb2233ca24fc18aee9a2c0
Shashi Nagarajan Iyer <shashiniyer@gmail.com>
2020-09-20 18:25:12 +0200

    corrected summary function to retain independent variable names + rewrote \VignetteIndexEntry to avoid warning

--- R/summary_linreg.R
+++ R/summary_linreg.R
@@ -7,6 +7,7 @@
 
 summary.linreg <- function(object, ...) {
   summ_df <- as.data.frame(object$coefficients)
+  summ_df[, 1] <- round(as.numeric(summ_df[, 1]), 5)
   summ_df[, 2] <- round(as.numeric(sqrt(diag(object$variance_of_coefficients))), 5)
   summ_df[, 3] <- round(as.numeric(object$t_values), 5)
   summ_df[, 4] <- sapply(object$p_values,
