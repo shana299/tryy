9c7c7a461f477f71100a08431671504d25b63f1c
Shashi Nagarajan Iyer <shashiniyer@gmail.com>
2020-09-20 17:54:48 +0200

    cleaned up summary function to round p_values to 5 decimals + changed \VignetteIndexEntry to avoid warning

--- R/summary_linreg.R
+++ R/summary_linreg.R
@@ -9,8 +9,14 @@ summary.linreg <- function(object, ...) {
   summ_df <- as.data.frame(object$coefficients)
   summ_df[, 2] <- round(sqrt(diag(object$variance_of_coefficients)), 5)
   summ_df[, 3] <- round(object$t_values, 5)
-  summ_df[, 4] <- sapply(object$p_values, 
-                         function(x) if(is.numeric(x)) {round(x, 5)} else {x})
+  summ_df[, 4] <- sapply(object$p_values,
+                         function(x) if (suppressWarnings(!is.na(as.numeric(x)))) {
+                           if (round(as.numeric(x), 5) == 0) {
+                             "<1e-5"
+                             } else {
+                               round(as.numeric(x), 5)
+                               }
+                           } else {x})
   summ_df[, 5] <- sapply(object$p_values, 
                          function(x) if(x < 0.001) {"***"} 
                          else if (x < 0.01) {"**"} 
